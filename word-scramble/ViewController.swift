b53d264511b888dbdac2804c4ef2b7c12f2e565f
Leo Liu <leo.liu@aequilibrium.ca>
2018-06-18 20:36:49 -0700

    tableview setup, startgame shuffles array

--- word-scramble/ViewController.swift
+++ word-scramble/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import GameplayKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    //MARK:properties
+    var allWords = [String]()
+    var usedWords = [String]()
+    var selectedWord:String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let startWordsPath = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsPath){
+                allWords = startWords.components(separatedBy: "\n")
+                //print(allWords)
+            }
+        }else{
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    //load new word
+    func startGame(){
+        //reshuffle array
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        selectedWord = allWords[0]
+        title = selectedWord
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {

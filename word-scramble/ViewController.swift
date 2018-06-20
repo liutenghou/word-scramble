9a03f24a2a3054da4b6e14c5908178772e5899d6
Leo Liu <leo.liu@aequilibrium.ca>
2018-06-19 19:10:24 -0700

    add closure for clicking alert

--- word-scramble/ViewController.swift
+++ word-scramble/ViewController.swift
@@ -19,6 +19,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //set the right nav bar button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordsPath = Bundle.main.url(forResource: "start", withExtension: "txt"){
             if let startWords = try? String(contentsOf: startWordsPath){
                 allWords = startWords.components(separatedBy: "\n")
@@ -31,7 +34,30 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    //load new word
+    //click the add button in nav
+    @objc func promptForAnswer(){
+        print("calling promptForAnswer")
+        
+        //closure example
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField() //adds an editable text field to alert
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default){
+            [unowned self, ac] (action:UIAlertAction) in
+                let answer = ac.textFields![0]
+                self.submit(answer: answer.text!)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    //click submit button from alert
+    func submit(answer: String){
+        print(answer)
+    }
+    
+    //reshuffles array, resets
     func startGame(){
         //reshuffle array
         allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
@@ -40,7 +66,19 @@ class ViewController: UITableViewController {
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
+    
+    //MARK: UITableView methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 
+    //MARK: UIViewController methods
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
